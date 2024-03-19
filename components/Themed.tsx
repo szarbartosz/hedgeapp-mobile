8c5f916b195d8aaefcc07bcd0f3aca99f0f75550
Bartosz Szar <szarbartosz@gmail.com>
2024-03-19 11:38:51 +0100

    Setup husky

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -6,6 +6,7 @@
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '@/constants/Colors';
+
 import { useColorScheme } from './useColorScheme';
 
 type ThemeProps = {
@@ -18,7 +19,7 @@ export type ViewProps = ThemeProps & DefaultView['props'];
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+  colorName: keyof typeof Colors.light
 ) {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
