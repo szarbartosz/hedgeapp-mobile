3f1eddb7f204ab787f1dc94c2224324b253de8b5
Bartosz Szar <szarbartosz@gmail.com>
2024-07-21 01:00:04 +0200

    feat: implement deadline countdown component

---
+++ src/app/components/deadline-countdown.tsx
@@ -0,0 +1,61 @@
+import { FC, useEffect, useState } from 'react';
+import { Text, useTheme } from 'tamagui';
+
+type Props = {
+  dates: string[];
+};
+
+const DeadlineCountdown: FC<Props> = ({ dates }) => {
+  const [daysLeft, setDaysLeft] = useState<number>();
+
+  const theme = useTheme();
+
+  useEffect(() => {
+    let minDaysLeft = Number.MAX_SAFE_INTEGER;
+
+    for (const date of dates) {
+      const parsedDate = new Date(date).getTime();
+
+      if (!isNaN(parsedDate)) {
+        const now = new Date().getTime();
+        const diffTime = parsedDate - now;
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+        minDaysLeft = Math.min(diffDays, minDaysLeft);
+      }
+    }
+
+    if (minDaysLeft !== Number.MAX_SAFE_INTEGER) {
+      setDaysLeft(minDaysLeft);
+    }
+  }, [dates]);
+
+  return (
+    daysLeft && (
+      <Text
+        color={
+          daysLeft > 90
+            ? theme.green10
+            : daysLeft > 60
+              ? theme.yellow10
+              : daysLeft > 30
+                ? theme.orange10
+                : daysLeft > 10
+                  ? theme.red10
+                  : theme.purple10
+        }>
+        {daysLeft > 1
+          ? `${daysLeft} dni`
+          : daysLeft === 1
+            ? `${daysLeft} dzień`
+            : daysLeft === 0
+              ? 'Dzisiaj!'
+              : daysLeft === -1
+                ? `${Math.abs(daysLeft)} dzień po terminie!`
+                : `${Math.abs(daysLeft)} dni po terminie!`}
+      </Text>
+    )
+  );
+};
+
+export default DeadlineCountdown;
