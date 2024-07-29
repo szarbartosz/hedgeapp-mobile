3e873d59f51c35fef02b6fe6c8481620f7f4cacb
Bartosz Szar <szarbartosz@gmail.com>
2024-07-30 00:22:19 +0200

    chore: extract investor contact to a separate component

--- src/app/investments/details.tsx
+++ src/app/investments/details.tsx
@@ -4,10 +4,10 @@ import { FC, useEffect, useRef, useState } from 'react';
 import { useColorScheme } from 'react-native';
 import MapView, { MapMarker } from 'react-native-maps';
 import Toast from 'react-native-toast-message';
-import { Button, H3, H4, ScrollView, Text, useTheme, View } from 'tamagui';
+import { Button, H3, H4, ScrollView, Text, useTheme } from 'tamagui';
 
-import { HardHatIcon, PhoneIcon } from '@/assets/icons';
 import DateIndicator from '@/components/date-indicator';
+import InvestorContact from '@/components/investor-contact';
 import LocalizationButton from '@/components/localization-button';
 import NavigationButton from '@/components/navigation-button';
 import darkMap from '@/utils/dark-map.json';
@@ -77,47 +77,7 @@ const InvestmentDetailsScreen: FC = () => {
           {`${investment?.address.city}, ${investment?.address.street} ${investment?.address.number}`}
         </Text>
 
-        <View
-          backgroundColor="$color1"
-          flex={1}
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between"
-          borderRadius={10}
-          borderWidth={1}
-          borderColor="$color5"
-          paddingVertical="$4"
-          paddingHorizontal="$3"
-          marginTop="$4">
-          <View
-            display="flex"
-            width="50%"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            gap={12}>
-            <HardHatIcon strokeColor={theme.color12.val} />
-            <View gap={4}>
-              <Text maxWidth={110} numberOfLines={1}>
-                {investment?.investor.name}
-              </Text>
-              <Text maxWidth={110} numberOfLines={1} color={theme.color11} fontSize={12}>
-                {investment?.investor.contactPerson}
-              </Text>
-            </View>
-          </View>
-          <View display="flex" height={46} borderWidth={0.5} borderColor={theme.color11.val}></View>
-          <View
-            display="flex"
-            width="50%"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            gap={12}>
-            <PhoneIcon strokeColor={theme.color12.val} />
-            <Text>Zadzwoń</Text>
-          </View>
-        </View>
+        {investment?.investor && <InvestorContact investor={investment?.investor} />}
 
         <H4 marginTop="$4">Terminy</H4>
         <DateIndicator
---
+++ src/components/investor-contact.tsx
@@ -0,0 +1,59 @@
+import { FC } from 'react';
+import { Text, useTheme, View } from 'tamagui';
+
+import { HardHatIcon, PhoneIcon } from '@/assets/icons';
+import { Investor } from '@/types/data';
+
+type Props = {
+  investor: Investor;
+};
+
+const InvestorContact: FC<Props> = ({ investor }) => {
+  const theme = useTheme();
+
+  return (
+    <View
+      backgroundColor="$color1"
+      flex={1}
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      borderRadius={10}
+      borderWidth={1}
+      borderColor="$color5"
+      paddingVertical="$4"
+      paddingHorizontal="$3"
+      marginTop="$4">
+      <View
+        display="flex"
+        width="50%"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        gap={12}>
+        <HardHatIcon strokeColor={theme.color12.val} />
+        <View gap={4}>
+          <Text maxWidth={110} numberOfLines={1}>
+            {investor.name}
+          </Text>
+          <Text maxWidth={110} numberOfLines={1} color={theme.color11} fontSize={12}>
+            {investor.contactPerson}
+          </Text>
+        </View>
+      </View>
+      <View display="flex" height={46} borderWidth={0.5} borderColor={theme.color11.val}></View>
+      <View
+        display="flex"
+        width="50%"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        gap={12}>
+        <PhoneIcon strokeColor={theme.color12.val} />
+        <Text>Zadzwoń</Text>
+      </View>
+    </View>
+  );
+};
+
+export default InvestorContact;
