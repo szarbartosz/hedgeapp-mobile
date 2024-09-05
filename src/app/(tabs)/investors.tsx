30c3af1134268c44fe5f9c42934ab245b9a50ffb
Bartosz Szar <szarbartosz@gmail.com>
2024-09-06 00:53:23 +0200

    feat: implement investors screen

--- src/app/(tabs)/investors.tsx
+++ src/app/(tabs)/investors.tsx
@@ -1,12 +1,54 @@
+import { router } from 'expo-router';
 import { FC } from 'react';
-import { H4, YStack } from 'tamagui';
+import { SafeAreaView } from 'react-native';
+import { Button, ListItem, ScrollView, useTheme, View, YGroup, YStack } from 'tamagui';
 
-const InvestorsScreen: FC = () => (
-  <YStack justifyContent="center" flexGrow={1}>
-    <H4 alignSelf="center" color="$color">
-      Inwestorzy
-    </H4>
-  </YStack>
-);
+import { useGetInvestorsQuery } from '@/api/investors.service';
+import { ChevronRightIcon } from '@/assets/icons';
+
+const InvestorsScreen: FC = () => {
+  const theme = useTheme();
+
+  const { data: investors, isSuccess } = useGetInvestorsQuery();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <YStack flex={1} marginBottom="$8" marginTop="$3">
+        <ScrollView>
+          <YGroup borderRadius={0}>
+            {isSuccess &&
+              investors.map(investor => (
+                <YGroup.Item key={investor.id}>
+                  <ListItem
+                    onPress={() =>
+                      router.navigate({
+                        pathname: '/investors/details',
+                        params: { id: investor.id },
+                      })
+                    }
+                    hoverTheme
+                    backgroundColor={theme.$color4}
+                    borderBottomColor={theme.$color6}
+                    borderBottomWidth={1}
+                    title={investor.name}
+                    subTitle={investor.contactPerson}
+                    iconAfter={ChevronRightIcon}
+                  />
+                </YGroup.Item>
+              ))}
+          </YGroup>
+        </ScrollView>
+      </YStack>
+      <View paddingHorizontal="$4" position="absolute" bottom="$4" width="100%">
+        <Button
+          backgroundColor={theme.$color12}
+          color={theme.$color1}
+          onPress={() => router.navigate('/investors/add')}>
+          Dodaj inwestora
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default InvestorsScreen;
