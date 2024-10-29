b108f22ae2c9180145305e15b8541d3360af74ea
Bartosz Szar <szarbartosz@gmail.com>
2024-10-29 21:59:22 +0100

    feat: implement status button

---
+++ src/components/status-button.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import { Button, Text, useTheme } from 'tamagui';
+
+import { Status } from '@/models/statuses';
+import { getStatusIcon } from '@/utils/helpers';
+
+type Props = {
+  status: Status;
+};
+
+const StatusButton: FC<Props> = ({ status }) => {
+  const theme = useTheme();
+
+  return (
+    <Button
+      onPress={() => {}}
+      style={{
+        width: 'fit-content',
+        height: 50,
+        borderRadius: 8,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.color4.val,
+        borderWidth: 1.5,
+        borderColor: theme.color10.val,
+        top: 190,
+        right: 10,
+        position: 'absolute',
+        zIndex: 10,
+      }}
+      icon={getStatusIcon(status.id)}>
+      <Text fontSize={16}>{status.name}</Text>
+    </Button>
+  );
+};
+
+export default StatusButton;
